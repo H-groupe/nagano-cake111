9316711b6859cb1401fb3cd2050bbb94cb386df4
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-18 11:25:14 +0000

    マイページ、退会確認の作成・編集TOP画面に遷移できるようにしたよ！

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,6 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
    @customer = current_customer
   end
@@ -6,15 +8,15 @@ class Public::CustomersController < ApplicationController
   def edit
    @customer = current_customer
   end
-  
+
   def update
     customer = current_customer
     customer.update(customer_params)
     redirect_to customers_path
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:family_name,
                                      :first_name,
