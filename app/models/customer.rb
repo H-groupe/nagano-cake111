209a8420ebd77cb216088c3d5a4ebfa7fea75b75
ayame <144100281+Ayame918@users.noreply.github.com>
2023-11-18 16:31:13 +0000

    管理者側の会員情報　controller.index.show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,29 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  
+  def full_name #結合してフルネームを返す
+    family_name + '' + first_name
+  end
+  
+  def full_name_kana #結合してフルネームを返す
+    family_name_kana + '' + first_name_kana
+  end
+  
+  def customer_status
+    if is_deleted == true
+      "退会"
+    else
+      "有効"
+    end
+  end
+  
+  def active_for_authentication? #退会したユーザーがログインできないようにするためのメソッド
+    super && (is_deleted == false) #deviseの認証が有効で、かつユーザーが退会していない状態（is_deletedがfalse`）であるユーザーだけが認証できる
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,15 +20,15 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
-    get '/customers/information/edit' => 'customers#edit',as: 'customers_edit'
-    patch '/customers/information' => 'customers#update',as: 'customers_update'
+    get 'customers/information/edit' => 'customers#edit', as: 'customers_edit'
+    patch 'customers/information' => 'customers#update', as: 'customers_update'
     resources :addresses, only: [:index, :edit]
     resources :orders, only: [:new, :index, :show]
     resources :cart_items, only: [:index, :update, :create, :destroy_all]
     resource :customers, only: [:show] do
       collection do
         get 'check' => 'customers#check'
-        patch 'withdraw' => 'customers#withdarw'
+        patch 'withdraw' => 'customers#withdraw'
       end
     end
     resources :items, only: [:index, :show]
