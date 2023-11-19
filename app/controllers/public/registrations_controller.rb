9e68dce82ea9174cbf9c4a9a7557bfe745d10384
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-11-19 08:35:42 +0000

    ログイン遷移を可能にして新規登録した先をカスタマーのshowページに遷移するようにしていらない文を消しました。

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -10,6 +10,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
     customer_session_path
   end
   
+  def  after_sign_up_path_for(resource)
+    customers_path
+  end
+  
 
   protected
 
