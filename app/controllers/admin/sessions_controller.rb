155450cb5bf63d4d096832ce36e60fe6df0e1b45
ayame <144100281+Ayame918@users.noreply.github.com>
2023-11-19 05:17:12 +0000

    ヘッター部分

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,24 +2,23 @@
 
 class Admin::SessionsController < Devise::SessionsController
   
-  def create
-    self.resource = warden.authenticate!(auth_options)
+  def create # ユーザー認証を行い、リソースを取得
+    self.resource = warden.authenticate!(auth_options) # ログイン成功時のフラッシュメッセージを設定
     set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
+    sign_in(resource_name, resource)  # ユーザーをサインインさせる
  
-    if resource.admin?
+    if resource.admin? # もしユーザーが管理者であれば
       respond_with resource, location: after_sign_in_path_for(resource)
     else
-      sign_out(resource_name)
+      sign_out(resource_name) # 管理者以外の場合はログアウトし、エラーメッセージを表示してリダイレクトする
       flash[:alert] = "管理者以外はログインできません。"
       redirect_to new_customers_session_path
     end
   end
 
-  def destroy
-    # ログアウトする
+  def destroy # ログアウトする
     sign_out(resource_name)
-    set_flash_message! :notice, :signed_out if signed_in?
+    set_flash_message! :notice, :signed_out if signed_in? # ログアウト成功時のフラッシュメッセージを設定
   end
 
   # before_action :configure_sign_in_params, only: [:create]
