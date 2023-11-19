076304f642ffe5d525d1af16a2f8d93fc4731d99
ayame <144100281+Ayame918@users.noreply.github.com>
2023-11-19 16:04:36 +0000

    会員編集エラー中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,11 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def new
     @item = Item.new
     @genres = Genre.all
     @genre = Genre.new
   end
-  
+
   def create
     @item = Item.new(items_params)
     if @item.save
@@ -15,22 +15,22 @@ class Admin::ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def index
     @items = Item.all
-    
+
   end
 
   def show
     @item = Item.find(params[:id])
-    
+
   end
 
   def edit
      @item = Item.find(params[:id])
       @genres = Genre.all
   end
-  
+
   def update
      @item = Item.find(params[:id])
     if @item.update(items_params)
@@ -41,11 +41,11 @@ class Admin::ItemsController < ApplicationController
        render :edit
     end
   end
-  
+
  private
 
    def items_params
      params.require(:item).permit(:name, :introduction, :genre_id, :price_excluding_tax, :is_sales_status)
    end
-  
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,11 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+  attribute :phone_number, :string
+  attribute :postcode, :string
+  attribute :is_deleted, :boolean
+
+  
   def full_name #結合してフルネームを返す
     family_name + '' + first_name
   end
@@ -17,12 +22,26 @@ class Customer < ApplicationRecord
   end
   
   def customer_status
-    if is_deleted == true
-      "退会"
-    else
+    if self.is_membership_status
       "有効"
+    else
+      "退会"
     end
   end
+  
+  def update
+    if @customer.update_customer(customer_params)
+      redirect_to admin_customer_path(@customer), notice: '会員情報が更新されました。'
+    else
+      render :edit
+    end
+  end
+  
+  def customer_params
+    # 必要に応じて許可する属性を指定
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_deleted)
+  end
+
   
   #def active_for_authentication? #退会したユーザーがログインできないようにするためのメソッド
   #  super && (is_deleted == false) #deviseの認証が有効で、かつユーザーが退会していない状態（is_deletedがfalse`）であるユーザーだけが認証できる
