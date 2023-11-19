dbbe9288413877e00d9f801fee8bbc802c4264e8
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-19 11:04:45 +0000

    a

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,19 @@
 class Public::AddressesController < ApplicationController
+
   def index
+    @address= Address.new
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+
+  private
+  def addresses_params
+    params.require(:address).permit(:postal_code,
+                                    :address,
+                                    :name)
+  end
+
 end
