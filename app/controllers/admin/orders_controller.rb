43a654f950703c312aaaae151fe5cb12da9f524d
KokiOhara <144040393+k7901@users.noreply.github.com>
2023-11-21 05:52:51 +0000

    aa

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,14 @@
 class Admin::OrdersController < ApplicationController
+  
+  def index
+    @orders
+  end
+  
   def show
     @orders = current_customer.orders
+  end
+  
+  def update
+    
   end
 end
