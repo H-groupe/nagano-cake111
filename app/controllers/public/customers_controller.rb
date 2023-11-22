7013d75d0b07c309312dd8edae98e6374a156770
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-11-22 04:15:04 +0000

    editページもバリデーションかけれました。

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,9 +10,12 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
-   @customer = current_customer
-   @customer.update(customer_params)
-   redirect_to customers_show_path
+      @customer = current_customer
+      if @customer.update(customer_params)
+        redirect_to customers_show_path
+      else
+        render 'edit'
+      end
   end
 
   private
