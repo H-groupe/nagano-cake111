ef5f71eac12ee34d8419fba85c23eb93d7f2d881
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-11-22 07:17:12 +0000

    フラッシュmessage入れました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper ApplicationHelper
-  before_action :authenticate_customer!, except: [:top]
+  before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   
@@ -13,13 +13,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_up_path_for(resource)
-    customers_path
-  end
+  #def after_sign_up_path_for(resource)
+    #customers_path
+  #end
   
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+  #def after_sign_in_path_for(resource)
+    #root_path
+  #end
 
   def after_sign_out_path_for(resource)
     admin_session_path
