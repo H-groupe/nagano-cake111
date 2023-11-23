6b5c8fd481cb192a60cc8e021cd986a6a75411b3
ayame <144100281+Ayame918@users.noreply.github.com>
2023-11-23 10:39:18 +0000

    ヘッターは完成　会員注文履歴反映されない2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 before_action :authenticate_any!
   def authenticate_any!
     if request.path.match(/\/admin(\/)?/) #url判定(urlにadminを含んでいるか)
-     authenticate_admin! #adminログイン制限
+      authenticate_admin! #adminログイン制限
     elsif controller_name == 'items' || action_name == 'top' || action_name == 'about'
   #itemsコントローラーかtopアクションまたはaboutアクションが読み込まれるとき
       return # 処理を抜ける
