6a85c7123b2eab9fa36e738017e0def466129cde
KokiOhara <144040393+k7901@users.noreply.github.com>
2023-11-24 05:43:33 +0000

    ooooo

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,16 +1,19 @@
 class Admin::OrdersController < ApplicationController
 
-  def index
-    @order = Order
+  def show
+    @member = Customer.find(params[:id])
+    @orders = @member.orders
+    @order = Order.find(params[:id])
+    @order_history = @member.orders.order(created_at: :desc)
   end
 
-  def show
+  def edit
     @order = Order.find(params[:id])
   end
 
   def update
-     @order = Order.find(params[:id])
-     @order.update(order_params)
-       flash[:notice] = "情報が更新されました"
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+      flash[:notice] = "情報が更新されました"
   end
 end
