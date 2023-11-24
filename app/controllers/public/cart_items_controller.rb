6801f3ed840f9c208f0f0b1668d74e5980381e7b
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-11-24 03:35:09 +0000

    あ、public/ordersのレイアウトをpublic/customersでやってたごめん

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,10 +6,10 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.quantity += params[:cart_item][:quantity].to_i
-    　cart_item.save
-      redirect_to cart_items_path
+       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.quantity += params[:cart_item][:quantity].to_i
+       cart_item.save
+       redirect_to cart_items_path
       
     elsif @cart_item.save
           flash[:notice] = "カート内に商品が追加されました。"
