a794984ccabcca7fed327fc69f63fc0cc1735106
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-11-28 07:27:47 +0000

    a

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,5 @@
 class Public::ItemsController < ApplicationController
-  before_action :authenticate_customer!, only: [:show]
+  #before_action :authenticate_customer!, only: [:show]
 
   def index
     @items,@sort = get_items(params)
